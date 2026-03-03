4dacca24aff6742ee5c26cec2848cbc33331b9ea
Fortune505 <forturn397@gmail.com>
2026-03-03 14:10:41 +0800

    logika

--- Zadanie3/Program.cs
+++ Zadanie3/Program.cs
@@ -14,9 +14,18 @@ class BPLA
         Console.WriteLine("Введите текст для шифровки (замена муха на слона):");
         string text = Console.ReadLine();
 
-        string shifr = text.Replace("муха", "слон");
+        string shifr = LogicBPLA.GetMena(text);
         Console.WriteLine("Результат:");
         Console.WriteLine(shifr);
+    }
+}
+public class LogicBPLA
+{
+    
+    public static string GetMena(string text)
+    {
+        if (string.IsNullOrEmpty(text)) return "";
+        return text.Replace("муха", "слон");
     }
 }
 
