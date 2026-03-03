692814fa7eb062404a736adaf7d9edd5e6b81c3a
Fortune505 <forturn397@gmail.com>
2026-03-03 14:06:26 +0800

    Добавьте файлы проекта.

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+class B
+{
+    static void Main()
+    {
+        BPLA.Elllo();
+    }
+}
+class BPLA
+{
+    public static void Elllo()
+    {
+        Console.WriteLine("Введите текст для шифровки (замена муха на слона):");
+        string text = Console.ReadLine();
+
+        string shifr = text.Replace("муха", "слон");
+        Console.WriteLine("Результат:");
+        Console.WriteLine(shifr);
+    }
+}
+
